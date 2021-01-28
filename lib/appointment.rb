812fc66cfa72687bc42552884906c03eadb03403
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-28 12:25:57 -0600

    commit tree 8ea0c2b9e7fc49b52f2bc03466a23eb82122f799

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,16 @@
 class Appointment
     attr_accessor :date, :doctor, :patient
-
+  
     @@all = []
-
+  
     def initialize(date, patient, doctor)
-        @date = date
-        @doctor = doctor
-        @patient = patient
-        @@all << self
+      @date = date
+      @doctor = doctor
+      @patient = patient
+      @@all << self
     end
-
+  
     def self.all
-        @@all
+      @@all
     end
-end
+  end
